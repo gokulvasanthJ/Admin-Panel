5ac14d75c361bd9facfe74ade029088f18d8571e
gokulvasanthJ <gokul27mech@gmail.com>
2025-01-02 12:00:24 +0530

    push login and signup ui

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 // import Navbar from '../components/navbar/Navbar'
 import Conversion from '../conversion/Conversion'
 import RecentOrder from '../components/recentorders/RecentOrder'
-// import LogIn from '../components/loginform/LogIn'
-// import SignUp from '../components/signup/SignUp'
+import LogIn from '../components/loginform/LogIn'
+import SignUp from '../components/signup/SignUp'
 
 const Homepage = () => {
   return (<>
   
   
 {/* <Navbar/> */}
-{/* <SignUp/>
-<LogIn/> */}
+<SignUp/>
+<LogIn/>
 <Conversion/>
 <RecentOrder/>
 
